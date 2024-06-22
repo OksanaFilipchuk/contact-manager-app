fa8468d41e69735e553c6b1527c557ef8cfc34fd
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-06-23 00:55:37 +0300

    add contact view page

--- src/app/contacts-manager/components/contact-view/contact-view.component.ts
+++ src/app/contacts-manager/components/contact-view/contact-view.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LocalStorageService } from '../../services/local-storage.service';
-import { ContactDataToSave } from '../../models/contact.model';
+import { ContactData } from '../../models/contact.model';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-contact-view',
@@ -10,19 +13,39 @@ import { ContactDataToSave } from '../../models/contact.model';
 })
 export class ContactViewComponent implements OnInit {
   contactId: string | null = null;
-  contact: ContactDataToSave;
+  contact: ContactData | null;
 
   constructor(
     private route: ActivatedRoute,
-    private localStorage: LocalStorageService
+    private router: Router,
+    private localStorageService: LocalStorageService,
+    private matdialog: MatDialog
   ) {}
 
   ngOnInit(): void {
     this.contactId = this.route.snapshot.paramMap.get('id');
     if (this.contactId) {
       this.contact = JSON.parse(
-        this.localStorage.getItem(this.contactId) || ''
+        this.localStorageService.getItem(this.contactId) || ''
       );
     }
+  }
+
+  openDialog() {
+    this.matdialog
+      .open(ConfirmDialogComponent, {
+        data: {
+          question: 'Are you sure you want to delete this contact?',
+          title: 'Delete contact',
+        },
+      })
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((res) => {
+        if (res && this.contactId) {
+          this.localStorageService.removeItem(this.contactId.toString());
+          this.router.navigate(['contacts']);
+        }
+      });
   }
 }
